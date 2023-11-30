12130670d11148766306e9afc9557597894fde94
SEBASTIEN KOENIG <sebastien.koenig@orange.fr>
2023-11-30 11:37:22 +0100

    correctrion footer mobile,  optimiser des performances

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,11 +5,12 @@
  *
  */  
 
-add_action( 'wp_enqueue_scripts', 'planty_style' );
-				function planty_style() {
-					wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-					wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
-				}
+ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+ function theme_enqueue_styles()
+ {
+	 wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	 wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+ }
 add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
 				function add_admin_link( $items, $args ) {
 					if (is_user_logged_in()) {
